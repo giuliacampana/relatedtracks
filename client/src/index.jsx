3fe05e3530143a23d9ba6d27c0f0db67bcab9247
Giulia Campana <giuliaecampana@gmail.com>
2018-06-11 10:29:38 -0700

    Remove deleted files from git workflow

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import css from '../../public/styles.css';
-import App from './App';
+import App from './components/App';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
